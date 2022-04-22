00095eb896a94857d4758abaa7d44c1e77805a5b
Ben Feinstein <feinsteinben@gmail.com>
2022-04-22 16:02:27 +0300

    Changes

---
+++ applications.py
@@ -0,0 +1,111 @@
+import math
+from typing import Tuple
+
+import torch
+
+import gpnn
+import resize_right
+
+_INF = float('inf')
+
+
+def image_generation(image: torch.Tensor,
+                     noise_std: float = 0.75,
+                     alpha: float = _INF,
+                     patch_size: int = 7,
+                     downscale_ratio: float = 0.75,
+                     num_levels: int = 9,
+                     reduce: str = 'weighted_mean') -> torch.Tensor:
+    pyramid = gpnn.make_pyramid(image, num_levels, downscale_ratio)
+    initial_guess = pyramid[-1] + noise_std * torch.randn_like(pyramid[-1])
+    return gpnn.gpnn(pyramid,
+                     initial_guess,
+                     alpha=alpha,
+                     downscale_ratio=downscale_ratio,
+                     patch_size=patch_size,
+                     reduce=reduce)
+
+
+def image_editing(source_image: torch.Tensor,
+                  edited_image: torch.Tensor,
+                  alpha: float = _INF,
+                  patch_size: int = 7,
+                  downscale_ratio: float = 0.75,
+                  num_levels: int = 5,
+                  reduce: str = 'weighted_mean') -> torch.Tensor:
+    source_pyramid = gpnn.make_pyramid(source_image, num_levels,
+                                       downscale_ratio)
+    edited_pyramid = gpnn.make_pyramid(edited_image, num_levels,
+                                       downscale_ratio)
+    initial_guess = edited_pyramid[-1]
+    return gpnn.gpnn(source_pyramid,
+                     initial_guess,
+                     alpha=alpha,
+                     downscale_ratio=downscale_ratio,
+                     patch_size=patch_size,
+                     reduce=reduce)
+
+
+def conditional_inpainting(masked_image: torch.Tensor,
+                           mask: torch.Tensor,
+                           alpha: float = _INF,
+                           patch_size: int = 7,
+                           downscale_ratio: float = 0.75,
+                           num_levels: int = 5,
+                           reduce: str = 'weighted_mean') -> torch.Tensor:
+    pyramid = gpnn.make_pyramid(masked_image, num_levels, downscale_ratio)
+    mask_pyramid = gpnn.make_pyramid(mask.to(masked_image), num_levels,
+                                     downscale_ratio)
+    initial_guess = pyramid[-1]
+    return gpnn.gpnn(pyramid,
+                     initial_guess,
+                     mask_pyramid=mask_pyramid,
+                     alpha=alpha,
+                     downscale_ratio=downscale_ratio,
+                     patch_size=patch_size,
+                     reduce=reduce)
+
+
+def structural_analogy(source_image: torch.Tensor,
+                       structure_image: torch.Tensor,
+                       alpha: float = 5e-3,
+                       patch_size: int = 7,
+                       downscale_ratio: float = 0.75,
+                       num_levels: int = 5,
+                       reduce: str = 'weighted_mean') -> torch.Tensor:
+    source_pyramid = gpnn.make_pyramid(source_image, num_levels,
+                                       downscale_ratio)
+    structure_pyramid = gpnn.make_pyramid(structure_image, num_levels,
+                                          downscale_ratio)
+    output_pyramid_shape = [x.shape for x in structure_pyramid]
+    initial_guess = structure_pyramid[-1]
+    return gpnn.gpnn(source_pyramid,
+                     initial_guess,
+                     output_pyramid_shape=output_pyramid_shape,
+                     alpha=alpha,
+                     downscale_ratio=downscale_ratio,
+                     patch_size=patch_size,
+                     reduce=reduce)
+
+
+def retargeting(image: torch.Tensor,
+                retargeting_ratio: Tuple[float, float],
+                alpha: float = 1e-3,
+                patch_size: int = 7,
+                downscale_ratio: float = 0.75,
+                num_levels: int = 9,
+                reduce: str = 'weighted_mean'):
+    pyramid = gpnn.make_pyramid(image, num_levels, downscale_ratio)
+    retargeted_generated = resize_right.resize(image, retargeting_ratio)
+    retargeted_pyramid = gpnn.make_pyramid(retargeted_generated, num_levels,
+                                           downscale_ratio)
+    retargeted_pyramid_shape = [level.shape for level in retargeted_pyramid]
+    initial_quess = retargeted_pyramid[-1]
+    return gpnn.gpnn(pyramid,
+                     initial_quess,
+                     output_pyramid_shape=retargeted_pyramid_shape,
+                     alpha=alpha,
+                     downscale_ratio=downscale_ratio,
+                     patch_size=patch_size,
+                     reduce=reduce,
+                     num_iters_in_coarsest_level=10)
--- gpnn.py
+++ gpnn.py
@@ -2,7 +2,6 @@ import math
 from typing import List, Optional, Sequence, Tuple
 
 import torch
-from torch import nn
 from torch.nn import functional as F
 
 import fold
@@ -10,15 +9,16 @@ import image as image_utils
 import resize_right
 
 _MAX_MEMORY_SIZE = 1 << 30
+_INF = float('inf')
 
 
-def gpnn(pyramid: List[torch.Tensor],
+def gpnn(pyramid: Sequence[torch.Tensor],
          initial_guess: torch.Tensor,
          downscale_ratio: float = 0.75,
          patch_size: int = 7,
-         alpha: float = 10.0,
-         output_pyramid_shape: Optional[List[torch.Size]] = None,
-         mask_pyramid: Optional[List[torch.Tensor]] = None,
+         alpha: float = _INF,
+         output_pyramid_shape: Optional[Sequence[torch.Size]] = None,
+         mask_pyramid: Optional[Sequence[torch.Tensor]] = None,
          num_iters_in_level: int = 10,
          num_iters_in_coarsest_level: int = 1,
          reduce: str = 'weighted_mean') -> torch.Tensor:
@@ -28,7 +28,7 @@ def gpnn(pyramid: List[torch.Tensor],
         mask_pyramid = [None] * len(pyramid)
     generated = initial_guess
     coarsest_level = len(pyramid) - 1
-    print(initial_guess.shape)
+    print(f'initial guess shape: {initial_guess.shape}')
     image_utils.imshow(initial_guess)
     for level in range(coarsest_level, -1, -1):
         print(f'level: {level}')
@@ -45,7 +45,7 @@ def gpnn(pyramid: List[torch.Tensor],
         else:
             blurred = resize_right.resize(pyramid[level + 1],
                                           1 / downscale_ratio,
-                                          output_pyramid_shape[level])
+                                          pyramid[level].shape)
             for i in range(num_iters_in_level):
                 print(f'iteration: {i}')
                 generated = pnn(generated,
@@ -55,7 +55,7 @@ def gpnn(pyramid: List[torch.Tensor],
                                 patch_size=patch_size,
                                 alpha=alpha,
                                 reduce=reduce)
-        print(generated.shape)
+        print(f'output shape: {generated.shape}')
         image_utils.imshow(generated)
         if level > 0:
             generated = resize_right.resize(generated, 1 / downscale_ratio,
@@ -68,8 +68,13 @@ def pnn(query: torch.Tensor,
         value: torch.Tensor,
         mask: Optional[torch.Tensor] = None,
         patch_size: int = 7,
-        alpha: float = 10.0,
+        alpha: float = _INF,
         reduce: str = 'weighted_mean') -> torch.Tensor:
+    query = query.unsqueeze(0)
+    key = key.unsqueeze(0)
+    value = value.unsqueeze(0)
+    if mask is not None:
+        mask = mask.unsqueeze(0)
     query_patches = fold.unfold2d(query, patch_size)
     query_patches_column, query_patches_size = fold.view_as_column(
         query_patches)
@@ -77,18 +82,22 @@ def pnn(query: torch.Tensor,
     value_patches_column, _ = fold.view_as_column(
         fold.unfold2d(value, patch_size))
     if mask is not None:
+        mask = (mask > 0.5).to(query)
         mask_patches_column, _ = fold.view_as_column(
             fold.unfold2d(mask, patch_size))
-        unmasked_patches = mask_patches_column.sum(
+        valid_patches_mask = mask_patches_column.sum(
             dim=2) > mask_patches_column.shape[2] - 0.5
-        key_patches_column = key_patches_column[:, :, unmasked_patches]
-        value_patches_column = value_patches_column[:, :, unmasked_patches]
-    dist = compute_dist_matrix(query_patches_column, key_patches_column)
-    dist /= dist.min(dim=0, keepdim=True)[0] + alpha
-    indices = dist.argmin(dim=2)
-    out_patches_column = F.embedding(indices, value_patches_column.squeeze(0))
+        key_patches_column = key_patches_column.squeeze(0)[
+            valid_patches_mask.squeeze(0)].unsqueeze(0)
+        value_patches_column = value_patches_column.squeeze(0)[
+            valid_patches_mask.squeeze(0)].unsqueeze(0)
+    _, indices = find_normalized_nearest_neighbors(query_patches_column,
+                                                   key_patches_column, alpha)
+    out_patches_column = F.embedding(indices.squeeze(2),
+                                     value_patches_column.squeeze(0))
     out_patches = fold.view_as_image(out_patches_column, query_patches_size)
-    return fold.fold2d(out_patches, reduce=reduce)
+    output = fold.fold2d(out_patches, reduce=reduce)
+    return output.squeeze(0)
 
 
 def make_pyramid(image: torch.Tensor, num_levels: int,
@@ -123,55 +132,92 @@ def _find_tile_size(height: int, width: int, cell_size: int,
     return best_tile_height, best_tile_width
 
 
-# def _compute_dist_matrix(queries: torch.Tensor,
-#                          keys: torch.Tensor) -> torch.Tensor:
-#     """Computes a matrix of MSE between each query and each key."""
-#     return torch.cdist(queries, keys, p=2).pow(2) / queries.shape[-1]
-
-
 def _compute_dist_matrix(queries: torch.Tensor,
                          keys: torch.Tensor) -> torch.Tensor:
     """Computes a matrix of MSE between each query and each key."""
-    x2 = torch.einsum('bid,bid->bi',queries, queries).unsqueeze(2)
-    y2 = torch.einsum('bjd,bjd->bj', keys, keys).unsqueeze(1)
-    xy = torch.einsum('bid,bjd->bij', queries, keys)
-    return (x2 + y2 - 2 * xy) / queries.shape[-1]
+    # x2 = torch.einsum('bid,bid->bi',queries, queries).unsqueeze(2)
+    # y2 = torch.einsum('bjd,bjd->bj', keys, keys).unsqueeze(1)
+    # xy = torch.einsum('bid,bjd->bij', queries, keys)
+    # return (x2 + y2 - 2 * xy) / queries.shape[-1]
+    return torch.cdist(queries, keys, p=2).pow(2) / queries.shape[-1]
 
 
-def compute_dist_matrix(
-        queries: torch.Tensor,
-        keys: torch.Tensor,
-        max_memory_usage: int = _MAX_MEMORY_SIZE) -> torch.Tensor:
-    """Computes distance matrix between queries and keys.
-    
-    The distance is MSE between patches.
+def _find_tile_height(height: int, width: int, cell_size: int,
+                      max_tile_size: int) -> Tuple[int, int]:
+    row_size = width * cell_size
+    return min(height, (max_tile_size + row_size - 1) // row_size)
 
-    Args:
-        queries: A tensor of queries of shape (B, Q, D).
-        keys: A tensor of keys shape (B, K, D).
-    
-    Returns:
-        A distance matrix shape (B, Q, K) of L2 distances between queries and keys.
-    """
-    if queries.shape[0] != keys.shape[0]:
-        raise ValueError('qeuries and keys must have the same batch size.')
-    if queries.shape[2] != keys.shape[2]:
-        raise ValueError('qeuries and keys must have the same patch size.')
+
+def _slice_weights(weights: Optional[torch.Tensor], start: int,
+                   stop: int) -> Optional[torch.Tensor]:
+    if weights is None:
+        return None
+    if weights.shape[1] == 1:
+        return weights
+    return weights[:, start:stop, :]
+
+
+def _find_weighted_nearest_neighbors(
+    queries: torch.Tensor,
+    keys: torch.Tensor,
+    weights: Optional[torch.Tensor] = None
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    dists = _compute_dist_matrix(queries, keys)
+    if weights is not None:
+        dists *= weights
+    return dists.min(dim=2, keepdim=True)
+
+
+def find_weighted_nearest_neighbors(
+    queries: torch.Tensor,
+    keys: torch.Tensor,
+    weights: Optional[torch.Tensor] = None,
+    max_memory_usage: int = _MAX_MEMORY_SIZE,
+) -> Tuple[torch.Tensor, torch.Tensor]:
     size_in_bytes = torch.finfo(queries.dtype).bits // 8
     batch_size = queries.shape[0]
     num_queries = queries.shape[1]
     num_keys = keys.shape[1]
-    tile_height, tile_width = _find_tile_size(num_queries, num_keys,
-                                              size_in_bytes, max_memory_usage)
-    num_rows = (num_queries + tile_height - 1) // tile_height
-    num_cols = (num_keys + tile_width - 1) // tile_width
-    dist = queries.new_empty(size=(batch_size, num_queries, num_keys))
-    for row in range(num_rows):
-        for col in range(num_cols):
-            row_start, row_stop = row * tile_height, (row + 1) * tile_height
-            col_start, col_stop = col * tile_width, (col + 1) * tile_width
-            queries_tile = queries[:, row_start:row_stop]
-            keys_tile = keys[:, col_start:col_stop]
-            dist_tile = _compute_dist_matrix(queries_tile, keys_tile)
-            dist[:, row_start:row_stop, col_start:col_stop] = dist_tile
-    return dist
+    if weights is not None:
+        max_memory_usage //= 2
+    tile_height = _find_tile_height(num_queries, num_keys,
+                                    batch_size * size_in_bytes,
+                                    max_memory_usage)
+    values = []
+    indices = []
+    for start in range(0, num_queries, tile_height):
+        value, idx = _find_weighted_nearest_neighbors(
+            queries[:, start:start + tile_height], keys,
+            _slice_weights(weights, start, start + tile_height))
+        values.append(value)
+        indices.append(idx)
+    return torch.cat(values, dim=1), torch.cat(indices, dim=1)
+
+    # values = queries.new_zeros(size=(batch_size, num_queries, 1))
+    # indices = torch.zeros(size=(batch_size, num_queries, 1), dtype=torch.int64, device=queries.device)
+    # for start in range(0, num_queries, tile_height):
+    #     value, idx = _find_weighted_nearest_neighbors(
+    #         queries[:, start:start + tile_height], keys,
+    #         _slice_weights(weights, start, start + tile_height))
+    #     values[:, start:start + tile_height] = value
+    #     indices[:, start:start + tile_height] = idx
+    # return values, indices
+
+
+def find_normalized_nearest_neighbors(
+    queries: torch.Tensor,
+    keys: torch.Tensor,
+    alpha: float = _INF,
+    max_memory_usage: int = _MAX_MEMORY_SIZE,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    if alpha < _INF:
+        # compute min distance where queries<-keys, and keys<-queries
+        normalizer, _ = find_weighted_nearest_neighbors(
+            keys, queries, None, max_memory_usage)
+        normalizer += alpha
+        normalizer = 1 / normalizer
+        normalizer = normalizer.transpose(1, 2)  # "keys" <-> "queries"
+    else:
+        normalizer = None
+    return find_weighted_nearest_neighbors(queries, keys, normalizer,
+                                           max_memory_usage)
